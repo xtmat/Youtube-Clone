c0bd99c2c0036b8a61a487be84a0f9e139d90ed6
Atul Kumar <atulkumar281725379@gmail.com>
2025-03-31 22:41:47 +0530

    updated build_files.sh

--- backend/build_files.sh
+++ backend/build_files.sh
@@ -2,8 +2,6 @@ echo "Build Start"
 
 apt install python3-pip
 python3 -m ensurepip
-python3 -m pip install pipenv
-python3 -m pip install python-dotenv
-python3 -m pipenv install
+python3 -m pip install -r requirements.txt
 
 echo "Build End"
