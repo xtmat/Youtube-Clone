ca2f26ebc0a55a8b3d5a97ef963d171ad423c488
Atul Kumar <atulkumar281725379@gmail.com>
2025-03-31 20:25:50 +0530

    added build files and vercel.json

--- backend/build_files.sh
+++ backend/build_files.sh
@@ -1,8 +1,9 @@
 echo "Build Start"
 
-sudo apt install python3-pip
+apt install python3-pip
 python3 -m ensurepip
 python3 -m pip install pipenv
+python3 -m pip instal python-dotenv
 python3 -m pipenv install
 
 echo "Build End"
